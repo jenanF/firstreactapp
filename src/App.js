f763b6ab6fc752686f32692923ffb0c8074b779b
jenanF <108689302+jenanF@users.noreply.github.com>
2024-11-10 20:24:36 +0300

    done

--- src/App.js
+++ src/App.js
@@ -7,6 +7,22 @@ function App() {
       <h1>Sweet Bites</h1>
       <p>The Bakery of All Backeries!</p>
       <img src={bakery}></img>
+
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: 20, marginTop: 20 }}>
+
+        <div style={{ width: 200, height: 300 }}>
+          <img height={200} width={150} src='https://www.rainbownourishments.com/wp-content/uploads/2023/01/vegan-chocolate-chip-muffins-1..jpg'></img>
+          <h4>Vegan Chocolate Chip Muffins</h4>
+          <p>1.000 KD</p>
+        </div>
+
+
+        <div style={{ width: 200, height: 300 }}>
+          <img height={200} width={150} src='https://cravingsjournal.com/wp-content/uploads/2023/08/croissants-4.jpg'></img>
+          <h4>Butter <br /> Croissant</h4>
+          <p>0.800 KD</p>
+        </div>
+      </div>
     </div>
   );
 }
