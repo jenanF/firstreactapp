07744a04797e252b8c84d8aa60e6e9e28d9afea5
jenanF <108689302+jenanF@users.noreply.github.com>
2024-11-10 19:59:13 +0300

    solved!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import bakery from './images/bakery.jpg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Sweet Bites</h1>
+      <p>The Bakery of All Backeries!</p>
+      <img src={bakery}></img>
     </div>
   );
 }
