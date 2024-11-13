3138b3cc5958514a6ab9a1035adb5476efd1f823
jenanF <108689302+jenanF@users.noreply.github.com>
2024-11-13 18:17:26 +0300

    done

--- src/App.js
+++ src/App.js
@@ -3,68 +3,75 @@ import bakery from './images/bakery.jpg'
 import products from './products.js';
 import Product from './Components/ProductItem.js'
 import ProductList from './Components/ProductList.js';
+import Home from './Components/Home.js';
 
 
 
 function App() {
   return (
-    <div className="App">
-      <h1>Sweet Bites</h1>
-      <p>The Bakery of All Backeries!</p>
-      <img src={bakery}></img>
 
-      <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', padding: 20, marginTop: 20 }}>
-
-        <ProductList items={products} />
+    <Home />
 
 
+    // <div className="App">
+    //   <h1>Sweet Bites</h1>
+    //   <p>The Bakery of All Backeries!</p>
+    //   <img src={bakery}></img>
 
+    //   <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', padding: 20, marginTop: 20 }}>
 
-        {/* <Product /> */}
-
-
-        {/* {products.map((item) =>
+    //     <ProductList items={products} />
 
 
 
 
-
-          // <div style={{ width: 200, height: 300 }}>
-
-          //   <img height={200} width={150} src={(item.image)}></img>
-
-          //   <h4>{(item.name)}</h4>
-          //   <p>{(item.price)} KD</p>
-
-          // </div>
-
-        )} */}
+    /* <Product /> */
 
 
-        {/////////////////////////////////////////////////////////////////////////////////
-        }
+    /* {products.map((item) =>
+  
+  
+  
+  
+  
+            // <div style={{ width: 200, height: 300 }}>
+  
+            //   <img height={200} width={150} src={(item.image)}></img>
+  
+            //   <h4>{(item.name)}</h4>
+            //   <p>{(item.price)} KD</p>
+  
+            // </div>
+  
+          )} */
 
-        {/* <div style={{ width: 200, height: 300 }}>
-          <img height={200} width={150} src={(products[0].image)}></img>
-          <h4>{(products[0].name)}</h4>
-          <p>{(products[0].price)} KD</p>
-        </div>
+
+    /////////////////////////////////////////////////////////////////////////////////
 
 
-        <div style={{ width: 200, height: 300 }}>
-          <img height={200} width={150} src={(products[1].image)}></img>
-          <h4>{(products[1].name.slice(0, 6))} <br /> {(products[1].name.slice(6))}</h4>
-          <p>{(products[1].price)} KD</p>
-        </div>
+    /* <div style={{ width: 200, height: 300 }}>
+            <img height={200} width={150} src={(products[0].image)}></img>
+            <h4>{(products[0].name)}</h4>
+            <p>{(products[0].price)} KD</p>
+          </div>
+  
+  
+          <div style={{ width: 200, height: 300 }}>
+            <img height={200} width={150} src={(products[1].image)}></img>
+            <h4>{(products[1].name.slice(0, 6))} <br /> {(products[1].name.slice(6))}</h4>
+            <p>{(products[1].price)} KD</p>
+          </div>
+  
+          <div style={{ width: 200, height: 300 }}>
+            <img height={200} width={150} src={(products[2].image)}></img>
+            <h4>{(products[2].name.slice(0, pie - 1))} <br /> {(products[2].name.slice(pie))}</h4>
+  
+            <p>{(products[2].price)} KD</p>
+          </div> */
 
-        <div style={{ width: 200, height: 300 }}>
-          <img height={200} width={150} src={(products[2].image)}></img>
-          <h4>{(products[2].name.slice(0, pie - 1))} <br /> {(products[2].name.slice(pie))}</h4>
 
-          <p>{(products[2].price)} KD</p>
-        </div> */}
-      </div>
-    </div>
+    //   </div>
+    // </div>
   );
 }
 
---
+++ src/Components/Home.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import '../App.css';
+import bakery from '../images/bakery.jpg'
+import products from '../products.js';
+import Product from './ProductItem.js'
+import ProductList from './ProductList.js';
+
+const Home = () => {
+    return (
+        <header>
+            <div className="App">
+                <h1>Sweet Bites</h1>
+                <p>The Bakery of All Backeries!</p>
+                <img src={bakery}></img>
+
+                <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', padding: 20, marginTop: 20 }}>
+
+                    <ProductList items={products} />
+
+                </div>
+            </div>
+
+        </header>
+    )
+}
+
+export default Home
